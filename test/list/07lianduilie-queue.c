68b246f01ebb4d08e2284b1755498180922be523
zhengying <zy840055899@163.com>
2016-01-20 22:15:56 +0800

    链队列最终提交

--- test/list/07lianduilie-queue.c
+++ test/list/07lianduilie-queue.c
@@ -60,10 +60,48 @@ void enQueue(LinkQueue *queue, int val){
     queue->rear = new;
 }
 
+//书上写的遍历
+void queueTraverse(LinkQueue queue){
+    PNode tmp = queue.front->pNext;
+    while (tmp != NULL) {
+        printf("%d ", tmp->data);
+        tmp = tmp->pNext;
+    }
+    printf("\n");
+}
+
+//我写的遍历方法（错误:链表的头结点数据域是空，只能显示0，少遍历一次）
+void queueTraverse2(LinkQueue queue){
+    PNode tmp = queue.front;
+    while (tmp != queue.rear) {
+        printf("%d ", tmp->data);
+        tmp = tmp->pNext;
+    }
+    printf("\n");
+}
+
+void clearQueue(LinkQueue *queue){
+    PNode tmp = queue->front->pNext;
+    PNode toClear;      //待释放空间
+    while (tmp != NULL) {
+        toClear = tmp;
+        tmp = tmp->pNext;
+        free(toClear);
+    }
+    //这一步不可少，空队列的必备条件
+    queue->rear = queue->front;
+    printf("\n");
+}
+
+void destroyQueue(LinkQueue *queue){
+    while (queue->front != NULL) {
+        queue->rear = queue->front->pNext;
+        free(queue->front);
+        queue->front = queue->rear;
+    }
+}
+
 int main(){
-    
-    printf("asdas\n");
-    
     LinkQueue queue;
     initQueue(&queue);
     
@@ -74,8 +112,27 @@ int main(){
 
     //进链队列
     enQueue(&queue, 99);
+    enQueue(&queue, -1);
+    enQueue(&queue, 3);
+    printf("入队三个元素，队列的长度为:%d\n", getQueueLength(queue));
+
+    //遍历队列
+    printf("正确的遍历：");
+    queueTraverse(queue);
+    printf("错误的遍历：");
+    queueTraverse2(queue);          //error
+    
+    //清空队列
+    clearQueue(&queue);
     printf("队列的长度为:%d\n", getQueueLength(queue));
 
-}
+    enQueue(&queue, 999);           //本行不报错，队列存在长度为空
+    printf("队列的长度为:%d\n", getQueueLength(queue));
+    //输出123123 12312312（地址）
+    printf("front:%u rear:%u front->pNext:%u \n", queue.front, queue.rear, queue.front->pNext);
 
+    destroyQueue(&queue);
+    printf("front:%u rear:%u \n", queue.front, queue.rear);     // 0 0
+//    enQueue(&queue, 990);           //本行报错，队列已不存在，入队失败
+}
 
