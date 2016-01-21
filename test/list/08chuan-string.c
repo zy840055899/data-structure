535df82f75e50345bca5eed6818c9e2ed877e520
zhengying <zy840055899@163.com>
2016-01-21 16:06:10 +0800

    串代码提交

--- test/list/08chuan-string.c
+++ test/list/08chuan-string.c
@@ -9,35 +9,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "string.h"
-
-
 #define MAXSIZE 40
 
-        typedef char String[MAXSIZE+1];     //0号单元存放串的长度
+typedef char String[MAXSIZE + 1];     //0号单元存放串的长度
 
 //生成一个字符串(字符数组)
-        void strAssign(String str, char *chars){
+void strAssign(String str, char *chars) {
 
-if(strlen(chars) > MAXSIZE){
-return;
-}
-str[0] = strlen(chars);
-for (int i=0; i<str[0]; i++) {
-printf("%c ", *(chars+i));
-str[i+1] = *(chars+i);
-}
+    if (strlen(chars) > MAXSIZE) {
+        return;
+    }
+    str[0] = strlen(chars);
+    for (int i = 0; i < str[0]; i++) {
+        printf("%c ", *(chars + i));
+        str[i + 1] = *(chars + i);
+    }
 }
 
-int strLength(String str){
+int strLength(String str) {
     return str[0];
 }
 
-int main(){
+int main() {
 
     String s1;
     strAssign(s1, "abcd");
-
-
 
 
 }
